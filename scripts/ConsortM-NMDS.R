43a9f5a8fe9b6f95b2ee45054c5d5343e7b8a74b
Thomas Hitch <th.tomhitch@gmail.com>
2021-03-12 16:28:52 +0100

    Add files via upload

---
+++ scripts/ConsortM-NMDS.R
@@ -0,0 +1,147 @@
+#!/usr/bin/env Rscript
+
+
+
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("This script required three inputs; data file, grouping file and project name.", call.=FALSE)
+} else if (length(args)==3) {
+
+
+
+##################################################################################
+######                          Load in libraries                           ######
+##################################################################################
+
+# Check if required packages are already installed, and install if missing
+#install.packages('vegan', repos='http://cran.us.r-project.org')
+#install.packages('ade4', repos='http://cran.us.r-project.org')
+#install.packages('adoni', repos='http://cran.us.r-project.org')
+
+
+
+library('vegan')
+library('ade4')
+#library('adoni')
+
+
+##################################################################################
+######                 User input required here parameter                   ######
+##################################################################################
+getwd()
+setwd(getwd())
+print(paste0("Current working dir: ", getwd()))
+
+
+maldi_data = args[1] # <-- Give the combined MALDI file here
+
+maldi_groups = args[2] # <-- Give the grouping file here
+
+plot_name = args[3] # <-- Change to the name you want for your plots
+
+
+
+##################################################################################
+######                         Automatic under here                         ######
+##################################################################################
+
+adonis.pair<-function(dist.mat,Factor,nper=1000,corr.method="fdr"){
+  require(vegan)
+  as.factor(Factor)
+  comb.fact<-combn(levels(Factor),2)
+  pv<-NULL
+  R2<-NULL
+  SS<-NULL
+  MeanSqs<-NULL
+  F.Model<-NULL
+  for (i in 1:dim(comb.fact)[2]){
+    model.temp<-adonis(as.dist(as.matrix(dist.mat)[Factor==comb.fact[1,i] | Factor==comb.fact[2,i],Factor==comb.fact[1,i] | Factor==comb.fact[2,i]])~Factor[Factor==comb.fact[1,i] | Factor==comb.fact[2,i]],permutations=nper)
+    pv<-c(pv,model.temp$aov.tab[[6]][1])
+    R2<-c(R2,model.temp$aov.tab$R2[1])
+    SS<-c(SS,model.temp$aov.tab[[2]][1])
+    MeanSqs<-c(MeanSqs,model.temp$aov.tab[[3]][1])
+    F.Model<-c(F.Model,model.temp$aov.tab[[4]][1])
+  }
+  pv.corr<-p.adjust(pv,method=corr.method)
+  data.frame(combination=paste(comb.fact[1,],comb.fact[2,],sep=" <-> "),SumsOfSqs=SS,MeanSqs=MeanSqs,F.Model=F.Model,R2=R2,P.value=pv,P.value.corrected=pv.corr)}
+
+##################################################################################
+######                         Reading in the data                          ######
+##################################################################################
+
+
+raw.data <- read.table (file = maldi_data , check.names = FALSE, header = TRUE, dec = ".", sep = ",", row.names = 1, comment.char = "")
+data <- t(raw.data)
+print(paste0("The MALDI file line number is; ", nrow(data)))
+print(row.names(data))
+
+Grouping <- read.table (file = maldi_groups, check.names = FALSE, header = TRUE, dec = ".", sep = "\t", row.names = 1, comment.char = "")
+print(paste0("The grouping file line number is; ", nrow(Grouping)))
+print(Grouping$Condition)
+
+cols <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000','lightblue','darkblue','lightgreen','darkgreen','pink','red','lightsalmon','orange','magenta','mediumpurple3','yellow','brown','black')
+
+
+##################################################################################
+######          Create basic plots for user assurance of quality            ######
+##################################################################################
+
+
+outfile <- sprintf("%s-bray_summed1.pdf",plot_name)
+pdf(outfile) 
+mds <- cmdscale(vegdist(data, method='bray'))
+plot(mds, type = 'n')
+points(mds, col = cols[Grouping$Condition], pch=16)
+points(mds, pch=1)
+legend('bottomleft', col=cols, legend=levels(Grouping$Condition), pch = 16, cex = 0.7)
+
+dev.off() 
+
+
+
+outfile <- sprintf("%s-morisita_summed1.pdf",plot_name)
+pdf(outfile) 
+mds <- cmdscale(vegdist(data, method='morisita'))
+plot(mds, type = 'n')
+points(mds, col = cols[Grouping$Condition], pch=16)
+points(mds, pch=1)
+legend('bottomleft', col=cols, legend=levels(Grouping$Condition), pch = 16, cex = 0.7)
+dev.off() 
+
+
+outfile <- sprintf("%s-bray-text.pdf",plot_name)
+pdf(outfile) 
+loc <- cmdscale(vegdist(data, method='bray')) 
+plot(mds, type="n", xlab="", ylab="")
+#points(mds, pch=1)
+#legend('bottomleft', col=cols, legend=levels(Grouping$Group), pch = 16, cex = 0.7)
+text(mds, rownames(loc), cex=0.4) 
+dev.off()
+
+
+##################################################################################
+######               Create Adonis plots for final user view                ######
+##################################################################################
+
+outfile <- sprintf("%s-bray-Adonis.pdf",plot_name)
+d_Binary_matrix<- as.matrix(vegdist(data, method='bray'))
+pdf(outfile) 
+meta <- metaMDS(d_Binary_matrix,k = 2)
+s.class(
+  meta$points, col = unique(cols), cpoint = 2, fac = Grouping$Condition,
+  sub = paste("metaNMDS plot stress is; ",formatC(meta[["stress"]], digits = 2)
+, "",sep="")
+)
+dev.off()
+
+
+##################################################################################
+######                      Provide pairwise statistics                     ######
+##################################################################################
+
+adonis.pair(as.dist(d_Binary_matrix), Grouping$Condition, nper = 1000, corr.method = "BH")
+
+}
+
---
+++ scripts/ConsortM.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python2.7
+
+import glob
+import argparse
+from subprocess import call
+import subprocess
+
+parser = argparse.ArgumentParser(description="Version 0.01. ConsortM provides simplified analysis of MALDI profiles. For further information go to the github; https://github.com/thh32/ConsortM") #simplifys the wording of using argparse as stated in the python tutorial
+
+# Basic input output files
+parser.add_argument("-i", type=str, action='store',  dest='input', help="Input folder containing sub-folders") # allows input of the forward read
+parser.add_argument("-p", type=str, action='store',  dest='project_name', help="Name of your project") # allows input of the forward read
+
+
+args = parser.parse_args()
+
+# Place each of the input into a simple variable to call
+input_files = str(args.input)
+project_name = str(args.project_name)
+
+
+print 'Input folder; ', input_files
+print 'Project name is; ', project_name
+
+
+print '1. Listing  files...'
+
+List_possibilities = []
+file_list = []
+
+for cfile in glob.glob(input_files + '/*/*'):
+    print 'Sample; ', cfile.split('/')[-2:][0]
+    file_list.append(cfile.split('/')[-2:][0])
+    dt = {}
+    for line in open(cfile,'r'):
+        timber = line.replace('\r\n','').split(' ')
+        List_possibilities.append(timber[0])
+Reduced_list = list(set(List_possibilities))
+
+print 'Total samples studied; ', len(file_list)
+print 'Total peaks studied; ', len(Reduced_list)
+
+
+print '1. Files listed OK.'
+
+
+
+
+data = {}
+
+for i in Reduced_list:
+    data[i] = []
+
+print '2. Reading in samples m/z values.'
+
+
+for cfile in glob.glob(input_files + '/*/*'):
+    print 'Sample; ', cfile.split('/')[-2:][0]
+    dt = {}
+    for line in open(cfile,'r'):
+        timber = line.replace('\r\n','').split(' ')
+        dt[timber[0]] = float(timber[1])
+        
+    for i in Reduced_list:
+        if i in dt.keys():
+            if i > 0.0:
+                prev = data[i]
+                prev.append(dt[i])
+                data[i] = prev
+            else:
+                prev.append(0.0)
+                data[i] = prev
+        else:
+            prev = data[i]
+            prev.append(0.0)
+            data[i] = prev
+
+print '2. All values stored in memory.'
+
+
+summed_1 = {}
+
+print '3. Reducing m/z values to integers.'
+
+
+for k,v in data.iteritems():
+    summ = round(float(k))
+    if summ in summed_1:
+        post = []
+        for t,y in zip(v, summed_1[summ]):
+            post.append(t+y)
+        summed_1[summ] = post
+    else:     
+        summed_1[summ] = v
+
+
+
+print '3. Reduction complete.'
+
+
+
+print '4. Outputting data.'
+
+
+
+outputting = open(project_name + '_data_summed1.csv','w')
+outputting_group = open(project_name + '_groups.csv','w')
+
+outputting_group.write('Name	Condition\n')
+
+outline = 'm/Z'
+file_num = {}
+
+numbs = 0
+for i in file_list:
+    numbs +=1
+    outline = outline + ',' + i.split('-')[0] + '-' + str(numbs)
+    outputting_group.write(i.split('-')[0] + '-' + str(numbs) + '\t' + i + '\n')
+
+outputting_group.close()
+
+
+outputting.write(outline + '\n')
+
+for k,v in summed_1.iteritems():
+    line = str(k)
+    for i in v:
+        if i < 0.0:
+            line = line + ',' + str(0.0)
+        else:
+            line = line + ',' + str(i)
+    outputting.write(line + '\n')
+    
+outputting.close()
+
+
+
+print '4. Outputting of combined files complete.'
+
+
+
+print '5. Running NMDS analysis .'
+
+bashCommand = 'Rscript ConsortM-NMDS.R ' +  project_name + '_data_summed1.csv ' + project_name + '_groups.csv ' + project_name
+print bashCommand
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+
+
+print '5. NMDS visualisation complete.'
+
+
+print 'Please cite out work at; .........'
+
+print 'For further sub-analysis, edit the combined files and run the command; ConsortM-NMDS.R Data_file Group_file Project_name'
+
+
+
+
